dbd52aa52c6663c0e30eddc1815075ddfd975757
THAM-SANQA <ndabat020@gmail.com>
2024-03-01 20:30:29 +0200

    tutorial

--- server.js
+++ server.js
@@ -16,10 +16,21 @@ app.use(bodyParser.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const db = require("./app/models");
+db.mongoose
+  .connect(db.url)
+  .then(() => {
+    console.log("Connected to the database!");
+  })
+  .catch(err => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
+  });
+
 // simple route
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome to TJ's application." });
-});
+// app.get("/", (req, res) => {
+//   res.json({ message: "Welcome to TJ's application." });
+// });
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
